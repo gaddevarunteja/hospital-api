6102d4815a7ddef3a4a1918ac143bc9a461aee0c
Varun <Varun@Varuns-MacBook-Pro.local>
2023-10-11 22:53:34 +0530

    Replaced deprecated code

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 const db = require('./config/mongoose');
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use('/', require('./routes'));
